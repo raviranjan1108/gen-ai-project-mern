60e9e53734ecb5f9b891af0ec78d5f8101210436
Ravi Ranjan Kumar <ranjanr50592@gmail.com>
2026-05-17 22:55:23 +0530

    fix: cors and cookie for production

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -61,7 +61,7 @@ async function registerUserController(req, res) {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 24 * 60 * 60 * 1000
         })
 
@@ -116,11 +116,10 @@ async function loginUserController(req, res) {
             { expiresIn: "1d" }
         )
 
-        res.cookie("token", token, {
+        res.clearCookie("token", {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge: 24 * 60 * 60 * 1000
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
 
         res.status(200).json({
